cb44f64e87948cb0a39c042466e250ee8e570450
PeterKampete <peterkampete01@gmail.com>
2022-04-10 16:29:01 +0100

    working on jumbotron

---
+++ src/containers/Jumbotron.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import JumboData from "../fixtures/jumbo.json";
+import { Jumbotron } from "../components/jumbotron";
+
+export const JumbotronContainer = () => {
+  return (
+    <Jumbotron.Container>
+      {JumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.Title>{item.title}</Jumbotron.Title>
+          <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          <Jumbotron.Image src={item.image} alt={item.alt} />
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+  );
+};
