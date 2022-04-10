78f00afcd98f1f24a85886b5305feb7a858ae2a2
PeterKampete <peterkampete01@gmail.com>
2022-04-10 17:00:42 +0100

    footer done! starting accordion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import FooterContainer from "./containers/footer";
 import { JumbotronContainer } from "./containers/Jumbotron";
 
 const App = () => {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 };
 
 export default App;
